bc33ef03e6ebeec9fb1fba97e11664e42ca2c528
Matt Dobson <mdobson4@gmail.com>
2014-10-29 08:04:52 -0400

    Initial commit.

---
+++ config.js
@@ -0,0 +1,29 @@
+var config = {
+  organization: 'dobson',
+  uri: 'http://dobson-test.apigee.net/a127-starter',
+  user: process.env.APIGEE_USER,
+  password: process.env.APIGEE_PASSWORD,
+  key: process.env.CONSUMER_KEY,
+  secret: process.env.CONSUMER_SECRET,
+  validGrantTypes: [ 'client_credentials' ],
+  passwordCheck: checkPassword
+};
+
+function checkPassword(username, password, cb){
+  return cb(null, true);
+}
+
+var Management = require('volos-management-apigee');
+var management = Management.create(config);
+
+var OAuth = require('volos-oauth-apigee');
+var oauth = OAuth.create(config);
+
+module.exports = {
+  managment: management,
+  oauth: oauth,
+  config: config,
+  localPort: 1337
+};
+
+
---
+++ server.js
@@ -0,0 +1,32 @@
+var zetta = require('zetta');
+var async = require('async');
+var OAuthExtension = require('zetta-volos-oauth');
+var config = require('./config');
+
+zetta()
+  .name('query.cloud')
+  .use(OAuthExtension(config))
+  .use(function(server) {
+    var mattsSecurityQuery = server.from('matt.dobson').where({ type: 'security-system' });
+    var detroitHubQuery = server.from('detroit').where({ type: 'huehub' });
+
+    server.observe([mattsSecurityQuery, detroitHubQuery], function(securitySystem, detroitHub) {
+      var blinkAndWait = function(callback) {
+        detroitHub.call('blink', function(err) {
+          callback(err);
+        });
+      };
+      securitySystem.on('alarm', function() {
+        async.times(3, function(n, next) {
+          blinkAndWait(next);
+        }, function(err) {
+          if(err) {
+            console.log('Error blinking:' + err);
+          }
+        });
+      });
+    });
+
+  })
+  .listen(1337);
+
